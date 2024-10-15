3375c4bffbe0ea5ca2280610f01e8443b6f5b3a9
yusufornek <yusufornek1225@gmail.com>
2024-10-15 11:18:01 +0300

    push

---
+++ mobileapp/week3/app.js
@@ -0,0 +1,6 @@
+function topla() {
+    var number1 = parseInt(document.getElementById('number1').value)|| 0; 
+    var number2 = parseInt(document.getElementById('number2').value)|| 0;
+    var result = number1 + number2; 
+    document.getElementById('result').value = result;
+}
